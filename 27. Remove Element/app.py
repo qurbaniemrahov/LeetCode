5ec2d6215d5348932c8e493e68b61b8b368e9750
qurbaniemrahov <qurbaniemrah@outlook.com>
2024-03-18 05:25:48 +0400

    updated

--- 27. Remove Element/app.py
+++ 27. Remove Element/app.py
@@ -1,14 +1,12 @@
 class Solution(object):
-    nums = [3, 2, 2, 3]
+    nums = [3, 2, 1, 3]
     val = 3
 
-    def removeElement(self, nums, val):
-        self.nums = nums
-        self.val = val
-        result = [num for num in nums if num != val]
-        return result
+    def removeElement(self):
+        self.nums = [num for num in self.nums if num != self.val]
+        return self.nums
 
 solution = Solution()
-result = solution.removeElement(solution.nums, solution.val)
-k = len(result)
-print(k)
+result = solution.removeElement()
+print(result)
+
