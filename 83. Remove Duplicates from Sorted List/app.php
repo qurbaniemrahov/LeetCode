da3cd0f10671e2f491900c47340990423a31233c
Qurbani Amrakhov <qurbaniemrah@outlook.com>
2025-01-05 11:50:38 +0400

    update

---
+++ 83. Remove Duplicates from Sorted List/app.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+ * Definition for a singly-linked list.
+ * class ListNode {
+ *     public $val = 0;
+ *     public $next = null;
+ *     function __construct($val = 0, $next = null) {
+ *         $this->val = $val;
+ *         $this->next = $next;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * A function to print and return "Hello World".
+     */
+    function deleteDuplicates($head) {
+       
+        return "Hello World"; // Return a value
+    }
+}
+
+$solution = new Solution();
+$head = null;
+$result = $solution->deleteDuplicates($head);
+echo $result; // Outputs "Hello WorldHello World"
+
+
+
+
+?>
